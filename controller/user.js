cf649c439daab640a42ee9d6e5f0a1b88b5bef1d
Birinchi <Bijitashya.Birinchi@emc.com>
2019-02-12 12:16:56 +0530

    final API

--- controller/user.js
+++ controller/user.js
@@ -101,68 +101,96 @@ module.exports.create = (req, res) => {
   });
 };
 
+// module.exports.forgot = function(req, res) {
+//   async.waterfall(
+//     [
+//       function(done) {
+//         crypto.randomBytes(20, function(err, buf) {
+//           var token = buf.toString('hex');
+//           done(err, token);
+//         });
+//       },
+//       function(token, done) {
+//         User.findOne({ email: req.body.email }, function(err, user) {
+//           if (!user) {
+//             return res.json({
+//               msg: 'No account with that email address exists.'
+//             });
+//           }
+
+//           user.resetPasswordToken = token;
+//           user.resetPasswordExpires = Date.now() + 3600000;
+
+//           user.save(function(err) {
+//             done(err, token, user);
+//           });
+//         });
+//       },
+//       function(token, user, done) {
+//         var smtpTransport = nodemailer.createTransport({
+//           service: 'Gmail',
+//           auth: {
+//             user: 'email',
+//             pass: 'password'
+//           }
+//         });
+//         var mailOptions = {
+//           to: user.email,
+//           from: 'AuthAPI',
+//           subject: 'Password Reset',
+//           text:
+//             'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+//             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+//             'http://' +
+//             req.headers.host +
+//             '/reset/' +
+//             token +
+//             '\n\n' +
+//             'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+//         };
+//         smtpTransport.sendMail(mailOptions, function(err, res) {
+//           console.log('mail sent');
+//           res.status(200).json({
+//             status: 'success',
+//             msg:
+//               'An e-mail has been sent to ' +
+//               user.email +
+//               ' with further instructions.'
+//           });
+//           done(err, 'done');
+//         });
+//       }
+//     ],
+//     function(err) {
+//       return res.status(422).json({ message: err });
+//     }
+//   );
+// };
+
 module.exports.forgot = function(req, res) {
-  async.waterfall(
-    [
-      function(done) {
-        crypto.randomBytes(20, function(err, buf) {
-          var token = buf.toString('hex');
-          done(err, token);
-        });
-      },
-      function(token, done) {
-        User.findOne({ email: req.body.email }, function(err, user) {
-          if (!user) {
-            return res.json({
-              msg: 'No account with that email address exists.'
-            });
-          }
-
-          user.resetPasswordToken = token;
-          user.resetPasswordExpires = Date.now() + 3600000;
-
-          user.save(function(err) {
-            done(err, token, user);
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          return res.json({
+            msg: 'No account with that email address exists.'
           });
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000;
+
+        user.save(function(err, res) {
+          done(err, token, user);
         });
-      },
-      function(token, user, done) {
-        var smtpTransport = nodemailer.createTransport({
-          service: 'Gmail',
-          auth: {
-            user: 'email',
-            pass: 'password'
-          }
-        });
-        var mailOptions = {
-          to: user.email,
-          from: 'AuthAPI',
-          subject: 'Password Reset',
-          text:
-            'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' +
-            req.headers.host +
-            '/reset/' +
-            token +
-            '\n\n' +
-            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function(err, res) {
-          console.log('mail sent');
-          res.status(200).json({
-            status: 'success',
-            msg:
-              'An e-mail has been sent to ' +
-              user.email +
-              ' with further instructions.'
-          });
-          done(err, 'done');
-        });
-      }
-    ],
-    function(err) {
-      return res.status(422).json({ message: err });
+        res.send({ token: token });
+      });
     }
-  );
+  ]);
 };
--- routes/user.js
+++ routes/user.js
@@ -3,75 +3,50 @@ var router = express.Router();
 var UserController = require('../controller/user');
 var nodemailer = require('nodemailer');
 var async = require('async');
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 router.post('/login', UserController.login);
 router.post('/create', UserController.create);
 router.post('/forgot', UserController.forgot);
 
 router.post('/reset/:token', function(req, res) {
-  async.waterfall(
-    [
-      function(done) {
-        User.findOne(
-          {
-            resetPasswordToken: req.params.token,
-            resetPasswordExpires: { $gt: Date.now() }
-          },
-          function(err, user) {
-            if (!user) {
-              return res.json({
-                msg: 'No account with that email address exists.'
+  async.waterfall([
+    function(done) {
+      User.findOne(
+        {
+          resetPasswordToken: req.params.token,
+          resetPasswordExpires: { $gt: Date.now() }
+        },
+        function(err, user) {
+          if (!user) {
+            return res.json({
+              msg: 'No account with that email address exists.'
+            });
+          }
+          if (req.body.password) {
+            bcrypt.genSalt(10, function(err, salt) {
+              bcrypt.hash(req.body.password, salt, function(err, hash) {
+                user.password = hash;
               });
-            }
-            if (req.body.password === req.body.confirm) {
-              user.setPassword(req.body.password, function(err) {
-                user.resetPasswordToken = undefined;
-                user.resetPasswordExpires = undefined;
+            });
 
-                user.save(function(err) {
-                  req.logIn(user, function(err) {
-                    done(err, user);
-                  });
-                });
-              });
-            } else {
-              return res.status(422).send({
-                message: err
-              });
-            }
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpires = undefined;
+
+            user.save(function(err) {
+              done(err, user);
+            });
+            res.send({ msg: 'reset successfull' });
+          } else {
+            return res.status(422).send({
+              message: err
+            });
           }
-        );
-      },
-      function(user, done) {
-        var smtpTransport = nodemailer.createTransport({
-          service: 'Gmail',
-          auth: {
-            user: 'email',
-            pass: 'password'
-          }
-        });
-        var mailOptions = {
-          to: user.email,
-          from: 'AuthAPI',
-          subject: 'Your password has been changed',
-          text:
-            'Hello,\n\n' +
-            'This is a confirmation that the password for your account ' +
-            user.email +
-            ' has just been changed.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function(err) {
-          res.status(200).json({
-            status: 'success'
-          });
-          done(err);
-        });
-      }
-    ],
-    function(err) {
-      return res.status(422).json({ message: err });
+        }
+      );
     }
-  );
+  ]);
 });
 
 module.exports = router;
